247782b3da5befaa2467814c2e3fe8882dec53ae
Isaac Watkins <iwatkins@nyc.yearup.org>
2019-11-20 16:53:24 -0500

    added new js that'll allow for calling on the api, need to add an interface, incoporate frame designs and restructure api call to async

--- main.js
+++ main.js
@@ -1,39 +1,76 @@
-const fetchPokemon = () => {
+// const fetchPokemon = () => {
+//
+//   const promises = []
+//   for(let poke = 1; poke <= 750; poke++) {
+//       const url = `https://fizal.me/pokeapi/api/v2/id/${poke}.json`
+//       promises.push(fetch(url).then((res) => res.json()))
+//
+//           .then(res => {
+//             return res.json()
+//     })
+//
+//       Promise.all(promises).then(results => {
+//         const pokemon = results.map(result => {
+//           name: data.name,
+//           id: data.id,
+//           image: data.sprites['front_default'],
+//           type: data.types.map((type) => type.type.name).join(', ')
+//         })
+//       })
+//       .then ((data) => {
+//         console.log(data)
+//
+//         const pokemon = {
+//           name: data.name,
+//           id: data.id,
+//           image: data.sprites['front_default'],
+//           type: data.types.map((type) => type.type.name).join(', ')
+//         }
+//         })
+//         console.log(pokemon)
+//       })
+// }
+//
+// const displayPokemon = (pokemon) => {
+//   console.log(pokemon)
+//   const html =
+// }
+//
+// fetchPokemon()
 
-  const promises = []
-  for(let poke = 1; poke <= 750; poke++) {
-      const url = `https://fizal.me/pokeapi/api/v2/id/${poke}.json`
-      promises.push(fetch(url).then((res) => res.json()))
+// let list = document.getElementById('poke-list')
+// id = prompt("Enter a pokemon's number")
+//
+// query = () => {
+//   fetch(`https://fizal.me/pokeapi/api/v2/id/${id}.json`)
+//   .then((res) => res.json())
+//   .then((data) => {
+//     console.log(data)
+//
+//     for (let i in data) {
+//       let li = document.createElement('li')
+//       li.innerText = `${id} ${data[i]['hp']} ${data[i]['']}`
+//       list.appendChild(li)
+//       }
+//     })
+//   }
 
-          .then(res => {
-            return res.json()
+
+  let list = document.getElementById('poke-list')
+
+  id = prompt("Enter a pokemon's number")
+
+  let li = document.createElement('li')
+
+  li.innerText = `${id} ${data[i]['hp']} ${data[i]['stats[5].basestat.']}`
+
+  list.appendChild(li)
+
+
+  query = () => {
+    fetch(`https://fizal.me/pokeapi/api/v2/id/${id}.json`)
+    .then((res) => res.json())
+    .then((data) => {
+      console.log(data)
     })
-
-      Promise.all(promises).then(results => {
-        const pokemon = results.map(result => {
-          name: data.name,
-          id: data.id,
-          image: data.sprites['front_default'],
-          type: data.types.map((type) => type.type.name).join(', ')
-        })
-      })
-      .then ((data) => {
-        console.log(data)
-
-        const pokemon = {
-          name: data.name,
-          id: data.id,
-          image: data.sprites['front_default'],
-          type: data.types.map((type) => type.type.name).join(', ')
-        }
-        })
-        console.log(pokemon)
-      })
-}
-
-const displayPokemon = (pokemon) => {
-  console.log(pokemon)
-  const html =
-}
-
-fetchPokemon()
+  }
