e78c6c740d4b2c1106ff16d53d5a44f43868477f
Stunnahizzy <56926442+Stunnahizzy@users.noreply.github.com>
2019-11-26 00:00:30 -0500

    added new logic and poke generator

--- main.js
+++ main.js
@@ -55,22 +55,102 @@
 //     })
 //   }
 
-
-  let list = document.getElementById('poke-list')
-
-  id = prompt("Enter a pokemon's number")
-
-  let li = document.createElement('li')
-
-  li.innerText = `${id} ${data[i]['hp']} ${data[i]['stats[5].basestat.']}`
-
-  list.appendChild(li)
+// ------- second try; OLDER logic -------
 
 
-  query = () => {
-    fetch(`https://fizal.me/pokeapi/api/v2/id/${id}.json`)
-    .then((res) => res.json())
-    .then((data) => {
-      console.log(data)
-    })
+// let list = document.getElementById('poke-list')
+//
+// id = prompt("Enter a pokemon's number")
+//
+// let li = document.createElement('li')
+//
+// li.innerText = `${id} ${data[i]['hp']} ${data[i]['stats[5].basestat.']}`
+//
+// list.appendChild(li)
+//
+//
+// query = () => {
+//   fetch(`https://fizal.me/pokeapi/api/v2/id/${id}.json`)
+//     .then((res) => res.json())
+//     .then((data) => {
+//       console.log(data)
+//     })
+// }
+
+// ----------------------- END ---------------------------------
+
+// ------------ michael quick js -----------
+
+
+// class pokemon {
+  //   constructor(abilities = []) {
+    //
+    //   }
+    //
+    //   addMoves(array) {
+      //     if (array.length == 0) return 'no moes in array!'
+      //     this.abilities = [...array]
+      //   }
+      //
+      // let pokemon = new pokemon()
+
+
+      /// -------- end -----------------------
+
+let pokeArray = []
+
+
+
+pokeAPI = async function pokeQuery() {
+
+  let num = prompt("Insert A Pokémon’s Number")
+  let req = await fetch(`https://fizal.me/pokeapi/api/v2/id/${num}.json`)
+  let data = await req.json()
+
+    pokemon = {
+
+      name: data.name,
+      pic: data.sprites.front_shiny,
+      stats: {
+          hp: data.stats[5].base_stat,
+          atk: data.stats[4].base_stat,
+          def: data.stats[3].base_stat
+        },
+
+      abl: data.abilities,
+
+   }
+
+
+  let makePoke =  () => {
+    let create = document.createElement("div")
+    document.body.appendChild(create)
+    create.innerHTML= `<img src = "${pokemon.pic}" width="100px" height="100px">
+     <p> <h3>${pokemon.name}</h3> <br>
+     ${pokemon.stats.hp} Hp<br>
+     ${pokemon.stats.def} Def <br>
+     ${pokemon.stats.atk} Atk <br>`
+     create.classname="generator"
+
   }
+  console.log(data)
+  pokeArray.push(pokemon)
+  makePoke(num)
+}
+
+
+
+
+//
+// class Pokemon {
+//   constructor(num) {
+//     pokemon = {
+//      this.name= data.name,
+//      this.pic= data.sprites.front_shiny,
+//      this.stats= {
+//          hp: data.stats[5].base_stat,
+//          atk: data.stats[4].base_stat,
+//          def: data.stats[3].base_stat
+//        },
+
+// let pokemon = new Pokemon(100);
